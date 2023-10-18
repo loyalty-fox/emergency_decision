87fee7175733ff5f4ea84eb2ad74f2b0bbfbeeb8
loyalty-fox <56210508+loyalty-fox@users.noreply.github.com>
2023-10-18 10:21:59 +0800

    Update module_1.py

--- module_1.py
+++ module_1.py
@@ -1,19 +1,27 @@
 from py2neo import Node,Relationship,Graph,Path,Subgraph,data
-# 本地neo4j数据库网址，全局用户名，自己设置的密码
-Connect to the database
+
+# Connect to the local database
+graph = Graph("bolt://localhost:7474", auth=("neo4j", "password"))
 
 Function cql_transfer(disaster_type_label)
     Construct query cql0 to retrieve relevant indicators m and n based on the user's input label
     Execute cql0 to obtain indicators m and n
     Prompt the user to input measurement data for m and n
     Divide m and n into ranges and convert them into corresponding strings
-    Construct a fuzzy query cql1 to find nodes related to disaster level based on the ranges, using the WITH keyword to retain those nodes (refer to relevant documentation)
-    Construct a fuzzy query cql2 to query the level of those nodes and find related rescue protocol nodes, returning personnel dispatch indicators (refer to relevant documentation)
-    cql = cql1 + cql2
+    Construct a fuzzy query cql_1 to find nodes related to disaster level based on the ranges, using the WITH keyword to retain those nodes (refer to relevant documentation)
+    Construct a fuzzy query cql_2 to query the level of those nodes and find related rescue protocol nodes, returning personnel dispatch indicators (refer to relevant documentation)
+    cql = cql_1 + cql_2
     Return cql
 
-Set disaster_type_label variable = User input("Enter the type of disaster:")
-Pass the label variable to cql_transfer function to generate the SQL statement my_sql
-Obtain query results: answers = run(my_sql).data()
+disaster_type_label = input("Enter the type of disaster:")
+my_sql = cql_transfer(disaster_type_label) 
 
-Print the answers in a readable format to provide information to the user
+# Obtain query results: 
+answers = run(my_sql).data()
+
+# Convert the query result into readable reference information
+print("\n#############################################\n")
+print("The {} level is:".format(label) + answers[0]['n.level'])
+print("Disaster grading criteria reference:" + answers[0]['n.reference document'])
+print("Number of rescue personnel to dispatch:" + answers[0]['m.personnel dispatch'])
+print("Personnel dispatch criteria reference:" + answers[0]['m.reference document'])
